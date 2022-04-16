106218b39a40d4e6382be7e6963abb10d7f7e6c7
sterlingh78 <sterlingh78@yahoo.com>
2022-04-16 17:31:51 -0600

    added final files

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+const hb = document.querySelector('#hamburgerBtn');
+
+hb.addEventListener('click', () => {
+    document.querySelector('#primaryNav').classList.toggle('open');
+})
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    speed: 2000,
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    // Enable Auto Play
+    autoplay: {
+        delay: 5000,
+    },
+  });
